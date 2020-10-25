ddacf7f28144958e2b77523851d01d8812e83ae1
zhf <755109759@qq.com>
2020-10-25 19:45:15 +0800

    算法，两数之和求下标

---
+++ src/com/letcode/easy/GetIndexNum.java
@@ -0,0 +1,62 @@
+package com.letcode.easy;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 给定一个整数数组 nums 和一个目标值 target，
+ * 请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+ * 示例：nums = [2, 7, 11, 15], target = 9
+ *      nums[0] + nums[1] = 2 + 7 = 9
+ *      所以返回 [0, 1]
+ */
+public class GetIndexNum {
+    public static void main(String[] args) {
+        int[] nums = {2,7,11,15};
+        int target = 13;
+        System.out.println(Arrays.toString(nums));
+        System.out.println(Arrays.toString(twoSum(nums, target)));
+
+    }
+
+    /**
+     * 时间复杂度：O(N)，其中 NNN 是数组中的元素数量。对于每一个元素 x，我们可以 O(1) 地寻找 target - x。
+     * 空间复杂度：O(N)，其中 NNN 是数组中的元素数量。主要为哈希表的开销。
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> hashtable = new HashMap<>();
+        for (int i = 0; i < nums.length; ++i) {
+            if (hashtable.containsKey(target - nums[i])) {
+                return new int[]{hashtable.get(target - nums[i]), i};
+            }
+            hashtable.put(nums[i], i);
+        }
+        return new int[0];
+    }
+
+    /**
+     * 双循环
+     * 时间复杂度：O(N^2)，其中N是数组中的元素数量。最坏情况下数组中任意两个数都要被匹配一次。
+     * 空间复杂度：O(1)
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum1(int[] nums, int target) {
+        int n = nums.length;
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[0];
+    }
+}
